34e9fecbc0abad9ca281c3da08260eb1ff417479
veltrup <veltrup@sitepark.com>
2022-12-22 08:09:35 +0100

    fix:initially without security
    Comes when the dependency on ies-security can be resolved.

--- extension-use-graphql/src/main/java/com/sitepark/ies/extension/examples/graphql/Query.java
+++ extension-use-graphql/src/main/java/com/sitepark/ies/extension/examples/graphql/Query.java
@@ -1,12 +1,9 @@
 package com.sitepark.ies.extension.examples.graphql;
 
-import com.sitepark.ies.extensions.graphql.api.annotations.Unsecured;
-
 import graphql.kickstart.tools.GraphQLQueryResolver;
 
 public class Query implements GraphQLQueryResolver {
 
-	@Unsecured
 	public String ping() {
 		return "pong";
 	}
