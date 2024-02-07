cda1d7cd0cd0c6650e794ae8f002619fa1150f3e
veltrup <veltrup@sitepark.com>
2024-02-07 17:19:56 +0100

    style: format with google java style guide

--- extension-use-events/src/main/java/com/sitepark/ies/extension/examples/useevents/UserPurgedSubsciber.java
+++ extension-use-events/src/main/java/com/sitepark/ies/extension/examples/useevents/UserPurgedSubsciber.java
@@ -1,37 +1,35 @@
 package com.sitepark.ies.extension.examples.useevents;
 
-import javax.inject.Inject;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import com.sitepark.ies.extension.api.annotations.Subscribe;
 import com.sitepark.ies.extension.api.events.UserPurged;
 import com.sitepark.ies.extension.api.port.EventBusSubscriber;
+import javax.inject.Inject;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class UserPurgedSubsciber {
 
-	private static final Logger LOGGER = LogManager.getLogger();
+  private static final Logger LOGGER = LogManager.getLogger();
 
-	private final EventBusSubscriber subscriber;
+  private final EventBusSubscriber subscriber;
 
-	@Inject
-	protected UserPurgedSubsciber(EventBusSubscriber subscriber) {
-		this.subscriber = subscriber;
-	}
+  @Inject
+  protected UserPurgedSubsciber(EventBusSubscriber subscriber) {
+    this.subscriber = subscriber;
+  }
 
-	public void subscribe() {
-		this.subscriber.register(this);
-	}
+  public void subscribe() {
+    this.subscriber.register(this);
+  }
 
-	public void unsubscribe() {
-		this.subscriber.unregister(this);
-	}
+  public void unsubscribe() {
+    this.subscriber.unregister(this);
+  }
 
-	@Subscribe
-	public void handle(UserPurged event) {
-		if (LOGGER.isInfoEnabled()) {
-			LOGGER.info("handle purge {}", event.getId());
-		}
-	}
+  @Subscribe
+  public void handle(UserPurged event) {
+    if (LOGGER.isInfoEnabled()) {
+      LOGGER.info("handle purge {}", event.getId());
+    }
+  }
 }
--- extension-use-graphql/src/main/java/com/sitepark/ies/extension/examples/graphql/GraphqlExtension.java
+++ extension-use-graphql/src/main/java/com/sitepark/ies/extension/examples/graphql/GraphqlExtension.java
@@ -1,33 +1,30 @@
 package com.sitepark.ies.extension.examples.graphql;
 
-import javax.inject.Inject;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import com.sitepark.ies.extension.api.ConfigurationContext;
 import com.sitepark.ies.extension.api.Extension;
 import com.sitepark.ies.extension.api.annotations.ConfigureFactory;
+import javax.inject.Inject;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class GraphqlExtension implements Extension {
 
-	private static final Logger LOGGER = LogManager.getLogger();
+  private static final Logger LOGGER = LogManager.getLogger();
 
-	@Inject
-	protected GraphqlExtension() {
-	}
+  @Inject
+  protected GraphqlExtension() {}
 
-	@ConfigureFactory
-	public static void configureInjection(ConfigurationContext configurationContext) {
-		if (LOGGER.isInfoEnabled()) {
-			LOGGER.info("configure {}", GraphqlExtension.class.getSimpleName());
-		}
-	}
+  @ConfigureFactory
+  public static void configureInjection(ConfigurationContext configurationContext) {
+    if (LOGGER.isInfoEnabled()) {
+      LOGGER.info("configure {}", GraphqlExtension.class.getSimpleName());
+    }
+  }
 
-	@Override
-	public void initialize() {
-		if (LOGGER.isInfoEnabled()) {
-			LOGGER.info("initialize {}", this.getClass().getSimpleName());
-		}
-	}
+  @Override
+  public void initialize() {
+    if (LOGGER.isInfoEnabled()) {
+      LOGGER.info("initialize {}", this.getClass().getSimpleName());
+    }
+  }
 }
